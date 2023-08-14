328a5028282a69b8768aa42eac10510687597568
Софія <sofialivin@gmail.com>
2023-08-14 16:21:42 +0200

    my homework

--- src/route/index.js
+++ src/route/index.js
@@ -8,11 +8,14 @@ const router = express.Router()
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/', function (req, res) {
+router.get('/selector', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
+  res.render('selector', {
+   
+  layout:'selector',
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
 
